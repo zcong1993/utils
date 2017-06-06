846cebecb4d8bcf87575969b57a88cad70a8656d
zcong1993 <1005997919@qq.com>
2017-06-06 16:06:17 +0800

    tweak

--- main.go
+++ main.go
@@ -9,29 +9,26 @@ import (
 // GetJson make a get request use http.Get
 func GetJson(url string, v interface{}) error {
 	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
 	defer resp.Body.Close()
-	return json.NewDecoder(resp.Body).Decode(v)
+	if err == nil {
+		err = json.NewDecoder(resp.Body).Decode(v)
+	}
+	return err
 }
 
 // GetJsonWithHeaders make a http get request with custom headers
 func GetJsonWithHeaders(url string, v interface{}, headers map[string]string) error {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, strings.NewReader(""))
-	if err != nil {
-		return err
-	}
 	for k, v := range headers {
 		req.Header.Set(k, v)
 	}
 	resp, err := client.Do(req)
-	if err != nil {
-		return err
-	}
 	defer resp.Body.Close()
-	return json.NewDecoder(resp.Body).Decode(v)
+	if err == nil {
+		err = json.NewDecoder(resp.Body).Decode(v)
+	}
+	return err
 }
 
 // SliceIndex return the index of slice item which match the condition first
